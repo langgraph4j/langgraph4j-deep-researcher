ad8e094908c31a17f0c14fb11ddf06354a2703e4
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-09-03 15:56:47 +0200

    feat(webui): add copilotkit integration

---
+++ webui/src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import Image from "next/image";
+import { SimpleChat } from "./component/chat";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <SimpleChat />
+    </main>
+  );
+}
